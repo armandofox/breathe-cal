a9c9ee92b8b5382b9ba83d6209d3871e2ef992aa
Jeffrey Spitz <jeffreyspitz99@berkeley.edu>
2017-10-26 17:54:01 +0000

    added to users model and set git to ignore secrets.yml

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161117071607) do
+ActiveRecord::Schema.define(version: 20171026170753) do
 
   create_table "cities", force: :cascade do |t|
     t.string   "name"
@@ -72,6 +72,8 @@ ActiveRecord::Schema.define(version: 20161117071607) do
     t.string   "title"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "provider"
+    t.string   "uid"
   end
 
 end
