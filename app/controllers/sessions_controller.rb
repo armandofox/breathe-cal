fe0a975104386923672adeee23a155be1e3de247
Jeffrey Spitz <jeffreyspitz99@berkeley.edu>
2017-10-27 01:32:25 +0000

    making progress on combining classes and user authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,21 +1,28 @@
 class SessionsController < ApplicationController
     # after_filter:database_cleanup
-    
-  def create 
-    test_check = params[:test_check]
-    if test_check
-      client = Client.new()
-      client.name = params[:name]
-      client.provider = 'some provider'
-      client.oauth_token = 'some token'
-      client.oauth_expires_at = Time.at(Time.new(2017, 10, 30))
-      client.save!
-    else
-      client = Client.from_omniauth(env["omniauth.auth"])
-    end 
-    session[:client_id] = client.id
+
+  def create
+    @user = User.find_or_create_from_auth_hash(auth_hash)
+    self.current_user = @user
+    session[:uid] = self.current_user.id
     redirect_to root_path
   end
+
+  # def create 
+  #   test_check = params[:test_check]
+  #   if test_check
+  #     client = Client.new()
+  #     client.name = params[:name]
+  #     client.provider = 'some provider'
+  #     client.oauth_token = 'some token'
+  #     client.oauth_expires_at = Time.at(Time.new(2017, 10, 30))
+  #     client.save!
+  #   else
+  #     client = Client.from_omniauth(env["omniauth.auth"])
+  #   end 
+  #   session[:client_id] = client.id
+  #   redirect_to root_path
+  # end
   
   def destroy
     session[:client_id] = nil
@@ -29,9 +36,11 @@ class SessionsController < ApplicationController
     end      
     render :json => data
   end
-  
-  def show
-    @client = Client.find(session[:client_id])
+
+  protected
+
+  def auth_hash
+    request.env['omniauth.auth']
   end
     
     
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,16 @@ class User < ActiveRecord::Base
     geocoded_by :address
     after_validation :geocode
     
-    def self.sign_in_from_omniauth(auth)
-        find_by(provider: auth["provider"], uid: auth["uid"]) || create_user_from_omniauth(auth)
+    def self.find_or_create_from_auth_hash(auth)
+        find_by(provider: auth[:provider], uid: auth[:uid]) || create_user_from_omniauth(auth)
     end
     
     def self.create_user_from_omniauth(auth)
-        create(provider: auth['provider'], 
-               uid: auth['uid'],
-               name: auth['name']
+        create(provider: auth[:provider], 
+               uid: auth[:uid],
+               name: auth[:info][:name],
+               oauth_token: auth[:credentials][:token],
+               oauth_expires_at: Time.at(auth[:credentials][:expires_at])
                )
     end
 end 
