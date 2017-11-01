f60eb45a5290ef1111a1ebe2f6a6f354fc92adea
Jeffrey Spitz <jeffreyspitz99@berkeley.edu>
2017-11-01 21:39:51 +0000

    this might fix it

--- db/schema.rb
+++ db/schema.rb
@@ -25,23 +25,9 @@ ActiveRecord::Schema.define(version: 20171026170753) do
     t.string   "lat"
     t.string   "lng"
     t.integer  "user_id"
-    t.integer  "client_id"
   end
 
-  add_index "cities", ["client_id"], name: "index_cities_on_client_id"
   add_index "cities", ["user_id"], name: "index_cities_on_user_id"
-
-  create_table "clients", force: :cascade do |t|
-    t.string   "name"
-    t.string   "email"
-    t.text     "searches"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
-    t.string   "provider"
-    t.string   "uid"
-    t.string   "oauth_token"
-    t.datetime "oauth_expires_at"
-  end
 
   create_table "markers", force: :cascade do |t|
     t.string   "lng"
@@ -57,12 +43,12 @@ ActiveRecord::Schema.define(version: 20171026170753) do
     t.boolean  "smoke"
     t.boolean  "gluten"
     t.boolean  "peanut"
-    t.integer  "client_id"
+    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  add_index "markers", ["client_id"], name: "index_markers_on_client_id"
+  add_index "markers", ["user_id"], name: "index_markers_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.float    "latitude"
