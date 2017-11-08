a70706f99bea60fa5c6d526eb68e7f2063190d95
Tinh Nguyen <tinh.t.nguyen@berkeley.edu>
2017-11-08 21:02:13 +0000

    [issue] javascript elements and rending is hanging up during capybara testing -> inconsistent test results

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -7,7 +7,6 @@
 When /^my location is set to "(.*)"$/ do |place| 
 <<<<<<< HEAD
   fill_in 'pac-input', with: place
-
   #find('#pac-input').set(place)
   find('#pac-input').native.send_keys(:Enter)
 =======
@@ -40,7 +39,9 @@ Then /I expect to see "(.*)" before "(.*)"/ do |e1, e2|
 end
 
 Then /I expect to see a list of cities$/ do
-  page.find('#list-box', visible: :all)
+  wait_for_ajax
+  list_box = page.find('#list-box', visible: :all)
+  puts list_box.text
 end
 
 Given(/^I touch the add marker CTA$/) do
@@ -127,8 +128,14 @@ Then /^I should see “.*?”$/ do |arg1|
 end
 
 Then /^I should see a link "(.*)"$/ do |link|
+  Capybara.ignore_hidden_elements = false
   wait_for_ajax
   expect find_link(link, visible: false)
+end
+
+Then /^I should not see a link "(.*)"$/ do |link|
+  wait_for_ajax
+  assert !find_link(link, visible: false)
 end
 
 Then /^I should see icon “.*?”$/ do |arg1|
