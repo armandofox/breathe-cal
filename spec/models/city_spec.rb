b729fb07cfc428e57759e1c1b173a56b75d936e1
Jeffrey Spitz <jeffreyspitz99@berkeley.edu>
2017-11-10 02:51:26 +0000

    finished rspec fixes

--- spec/models/city_spec.rb
+++ spec/models/city_spec.rb
@@ -5,7 +5,6 @@ RSpec.describe City, type: :model do
   
     it "gets the location key" do 
       # byebug
-      skip "revisit later"
       expect(City.get_loc_key("37.8716", "-122.2727", "Berkeley")).to eq("332044")
     end
     
