f1efdeb1a41167f23daaa52565119e47742fc86d
Jeffrey Spitz <jeffreyspitz99@berkeley.edu>
2017-11-20 09:15:01 +0000

    [feat] finished rspec tests for new application_controller methods + updated 500 error page text + updated user factory to include user ID

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -3,6 +3,7 @@ FactoryGirl.define do
     name 'John Smith' # default values
     provider 'google_oauth2'
     uid 101
+    id 1
     email "test@xxxx.com"
     oauth_token 'some_token'
     oauth_expires_at Time.now + 10.day
