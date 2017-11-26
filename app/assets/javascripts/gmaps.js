787bdb5be6a89e06e8b8fec1182e4932c4084ba3
Alex Beraut <alexberaut@gmail.com>
2017-11-26 21:09:23 +0000

    changed the closeclick addListener to addEventListener so it can be removed afterward

--- app/assets/javascripts/gmaps.js
+++ app/assets/javascripts/gmaps.js
@@ -252,7 +252,7 @@ function initAutocomplete() {
     recentMarker = marker;
     
     // Close the window and remove the created marker if the user exits
-    var listenerHandle = google.maps.event.addListener(infowindow, 'closeclick', function(){
+    var listenerHandle = google.maps.event.addEventListener(infowindow, 'closeclick', function(){
       labelNum -=1;
       recentMarker.setMap(null);
       recentMarker = null;
